e392553ed588d76bd48fcad4db2230977db4cc3f
Ms. Bean <spence12310@icloud.com>
2023-09-21 17:58:59 -0400

    Fixed port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const cors = require('cors')
+const mysql = require('mysql');
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3500
 
 app.use(cors({
     origin: '*'
