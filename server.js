aa369328c35e892a8fa090086bb57b5af175105d
Spencer Clarke <spence12310@icloud.com>
2023-10-11 10:52:38 -0400

    Added customer rental features, began implementing pdf report generation

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
 const http = require('http');
+
+
+const fs = require('fs');
+const PDFDocument = require('pdfkit');
+const path = require('path');
+
 const app = express();
 const PORT = 3500;
 
@@ -18,7 +24,7 @@ con.connect(function(err) {
             throw err;
     });
     console.log("connected to sakila database");
-});
+})
 
 /*Returns number of rows where value is found in column in table (used for input sanitization) */
 async function value_count_in_column(table_name, column_name, value)
@@ -153,7 +159,6 @@ app.get('/get_film_list', (req, res) => {
                 failure: 1,
                 message: 'Title can only contain letters and spaces'
             })
-            return;
         }
     }
     if(req.headers.actor_first_name)
@@ -164,7 +169,6 @@ app.get('/get_film_list', (req, res) => {
                 failure: 1,
                 message: 'Actor first name can only contain letters and hyphens'
             })
-            return;
         }
     }
     if(req.headers.actor_last_name)
@@ -175,7 +179,6 @@ app.get('/get_film_list', (req, res) => {
                 failure: 1,
                 message: 'Actor last name can only contain letters and hyphens'
             })
-            return;
         }
     }
     if(req.headers.genre)
@@ -186,7 +189,6 @@ app.get('/get_film_list', (req, res) => {
                 failure: 1,
                 message: 'Genre can only contain letters and spaces'
             })
-            return;
         }
     }
     var sql_string = "SELECT DISTINCT T3.film_id, T3.title, T3.name AS category, T3.description FROM (SELECT T2.film_id, T2.title, T2.name, T2.description, film_actor.actor_id FROM (SELECT T1.film_id, T1.title, T1.description, category.name FROM (SELECT film.film_id, film.title, film_category.category_id, film.description FROM film INNER JOIN film_category ON film.film_id = film_category.film_id) T1 INNER JOIN category ON T1.category_id = category.category_id) T2 INNER JOIN film_actor ON T2.film_id = film_actor.film_id) T3 INNER JOIN actor ON T3.actor_id = actor.actor_id WHERE 1=1";
@@ -230,7 +232,6 @@ app.get('/get_customer_list', (req, res) => {
                 failure: 1,
                 message: 'Customer ID must be a number'
             })
-            return;
         }
     }
     if(req.headers.customer_first_name)
@@ -241,7 +242,6 @@ app.get('/get_customer_list', (req, res) => {
                 failure: 1,
                 message: 'First name must contain only letters or hyphens'
             })
-            return;
         }
     }
     if(req.headers.customer_last_name)
@@ -252,7 +252,6 @@ app.get('/get_customer_list', (req, res) => {
                 failure: 1,
                 message: 'Last name must contain only letters or hyphens'
             })
-            return;
         }
     }
 
@@ -291,7 +290,6 @@ app.post("/add_customer", (req, res) => {
             failure: 1,
             message: 'Missing values. All values except email are required.'
         })
-        return;
     }
     if(!/^[0-9]*$/.test(req.headers.store_id) || !/^[0-9]*$/.test(req.headers.address_id))
     {
@@ -299,7 +297,6 @@ app.post("/add_customer", (req, res) => {
             failure: 1,
             message: 'Store id and address id must be numbers.'
         })
-        return;
     }
     if(!/^[a-zA-Z\-]*$/.test(req.headers.first_name) || !/^[a-zA-Z\-]*$/.test(req.headers.last_name))
     {
@@ -307,7 +304,6 @@ app.post("/add_customer", (req, res) => {
             failure: 1,
             message: 'First and last name must contain only letters and hyphens.'
         })
-        return;
     }
     if(req.headers.email)
     {
@@ -317,7 +313,6 @@ app.post("/add_customer", (req, res) => {
                 failure: 1,
                 message: 'The email must be a valid email.'
             })
-            return;
         }
     }
 
@@ -329,7 +324,6 @@ app.post("/add_customer", (req, res) => {
             failure: 1,
             message: 'The store id corresponds to a store that does not exist.'
         })
-        return;
     }
 
     /*Insertion*/
@@ -365,7 +359,6 @@ app.post("/update_customer", (req, res) => {
             failure: 1,
             message: 'Customer ID is needed to update customer information.'
         })
-        return;
     }
     if(!req.headers.store_id && !req.headers.first_name && !req.headers.last_name && !req.headers.address_id && !req.headers.email)
     {
@@ -373,7 +366,6 @@ app.post("/update_customer", (req, res) => {
             failure: 1,
             message: 'No values are set to be updated.',
         })
-        return;
     }
     if(req.headers.email)
     {
@@ -383,7 +375,6 @@ app.post("/update_customer", (req, res) => {
                 failure: 1,
                 message: 'The email must be a valid email.'
             })
-            return;
         }
     }
     if(req.headers.store_id)
@@ -394,7 +385,6 @@ app.post("/update_customer", (req, res) => {
                 failure: 1,
                 message: 'Store ID must be a number.',
             })
-            return;
         }
     }
     if(req.headers.first_name)
@@ -405,7 +395,6 @@ app.post("/update_customer", (req, res) => {
                 failure: 1,
                 message: 'First name must only contain letters and hyphens.',
             })
-            return;
         }
     }
     if(req.headers.last_name)
@@ -416,7 +405,6 @@ app.post("/update_customer", (req, res) => {
                 failure: 1,
                 message: 'Last name must only contain letters and hyphens.',
             })
-            return;
         }
     }
     if(req.headers.address_id)
@@ -427,7 +415,6 @@ app.post("/update_customer", (req, res) => {
                 failure: 1,
                 message: 'Address ID must be a number.',
             })
-            return;
         }
     }
     /*Verify that the database can handle the request*/
@@ -440,7 +427,6 @@ app.post("/update_customer", (req, res) => {
             failure: 1,
             message: 'Customer does not exist.',
         })
-        return;
     }
     get_value_where("customer", "customer_id", req.headers.customer_id, "active").then( function(customer_is_active){
     if(customer_is_active == 0)
@@ -449,7 +435,6 @@ app.post("/update_customer", (req, res) => {
             failure: 1,
             message: 'Customer does not exist.',
         })
-        return;
     }
     
     if(req.headers.store_id && store_count < 1)
@@ -458,7 +443,6 @@ app.post("/update_customer", (req, res) => {
             failure: 1,
             message: 'Store ID corresponds to a store that does not exist.',
         })
-        return;
     }
     var sql_string = "UPDATE customer SET ";
     if(req.headers.email)
@@ -508,7 +492,6 @@ app.post("/delete_customer", (req, res) => {
             failure: 1,
             message: 'Customer ID is needed to delete the customer.'
         })
-        return;
     }
     if(!/^[0-9]*$/.test(req.headers.customer_id))
     {
@@ -516,7 +499,6 @@ app.post("/delete_customer", (req, res) => {
             failure: 1,
             message: 'Customer ID needs to be a number.'
         })
-        return;
     }
     /*Verify that the customer exists*/
     value_count_in_column("customer", "customer_id", req.headers.customer_id).then( function(customer_count){
@@ -526,7 +508,6 @@ app.post("/delete_customer", (req, res) => {
             failure: 1,
             message: 'Customer does not exist.',
         })
-        return;
     }
     get_value_where("customer", "customer_id", req.headers.customer_id, "active").then( function(customer_is_active){
     if(customer_is_active == 0)
@@ -535,7 +516,6 @@ app.post("/delete_customer", (req, res) => {
             failure: 1,
             message: 'Customer does not exist.',
         })
-        return;
     }
     
     var sql_string = "";
@@ -554,4 +534,191 @@ app.post("/delete_customer", (req, res) => {
     });
     });
 })
+app.post("/rent_out_movie", (req, res) => {
+    /*Input sanitization*/
+    if(!req.headers.customer_id)
+    {
+        return res.status(200).send({
+            failure: 1,
+            message: 'Customer ID is needed to rent out the movie.'
+        })
+    }
+    if(!/^[0-9]*$/.test(req.headers.customer_id))
+    {
+        return res.status(200).send({
+            failure: 1,
+            message: 'Customer ID needs to be a number.'
+        })
+    }
+    if(!req.headers.film_id)
+    {
+        return res.status(200).send({
+            failure: 1,
+            message: 'Film ID is needed to rent out the movie.'
+        })
+    }
+    if(!/^[0-9]*$/.test(req.headers.film_id))
+    {
+        return res.status(200).send({
+            failure: 1,
+            message: 'Film ID needs to be a number.'
+        })
+    }
+    if(!req.headers.staff_id)
+    {
+        return res.status(200).send({
+            failure: 1,
+            message: 'Staff ID is needed to rent out the movie.'
+        })
+    }
+    if(!/^[0-9]*$/.test(req.headers.staff_id))
+    {
+        return res.status(200).send({
+            failure: 1,
+            message: 'Staff ID needs to be a number.'
+        })
+    }
+    if(!req.headers.store_id)
+    {
+        return res.status(200).send({
+            failure: 1,
+            message: 'Store ID is needed to rent out the movie.'
+        })
+    }
+    if(!/^[0-9]*$/.test(req.headers.film_id))
+    {
+        return res.status(200).send({
+            failure: 1,
+            message: 'Store ID needs to be a number.'
+        })
+    }    /*Verify that the database can handle the request*/
+
+    value_count_in_column("staff", "staff_id", req.headers.staff_id).then( function(staff_count){
+    if(staff_count == 0)
+    {
+        return res.status(200).send({
+            failure: 1,
+            message: 'Staff ID corresponds to a non-existent staff member.'
+        });
+    }
+
+    value_count_in_column("customer", "customer_id", req.headers.staff_id).then( function(customer_count){
+    if(customer_count == 0)
+    {
+        return res.status(200).send({
+            failure: 1,
+            message: 'Customer ID corresponds to a non-existent customer member.'
+        });
+    }
+    /*Get an inventory id of an item that is not already rented out*/
+    sql_string = "SELECT inventory_id, store_id FROM (SELECT inventory.inventory_id, inventory.store_id, COUNT(CASE WHEN rental.return_date IS NULL THEN 1 END) AS null_count FROM inventory INNER JOIN rental ON inventory.inventory_id = rental.inventory_id WHERE inventory.film_id = " + req.headers.film_id + " GROUP BY inventory.inventory_id) T1 WHERE T1.null_count = 0 AND T1.store_id = " + req.headers.store_id + " LIMIT 1";
+    con.query(sql_string, 
+    function (err, result_1, fields) {
+        if (err) 
+            throw err;
+        if(result_1.length == 0)
+        {
+            return res.status(200).send({
+                failure: 1,
+                message: "All inventory items with film_id " + req.headers.film_id + " have been rented out."
+            });
+        }
+        else
+        {
+            sql_string = "INSERT INTO rental (rental_date, inventory_id, customer_id, staff_id) VALUES (CURRENT_TIMESTAMP()," + result_1[0].inventory_id + "," + req.headers.customer_id + ", " + req.headers.staff_id + ");"; 
+            con.query(sql_string, 
+            function (err, result_2, fields) {
+                if (err) 
+                    throw err;
+                return res.status(200).send({
+                    failure: 0,
+                    message: "Film rented out."
+                });
+            });
+        }
+    })})});
+})
+
+app.get("/get_customer_rentals", (req, res) => {
+    if(!/^[0-9]+$/.test(req.headers.customer_id))
+    {
+        return res.status(200).send({
+            failure: 1,
+            message: 'Customer ID needs to be a number.'
+        });
+    }
+    sql_string = "SELECT customer.first_name, customer.last_name, rental.rental_id, rental.rental_date, rental.inventory_id, rental.return_date FROM rental INNER JOIN customer ON rental.customer_id = customer.customer_id WHERE customer.customer_id = " + req.headers.customer_id + ";";
+    con.query(sql_string, function (err, result, fields) {
+        if (err) 
+            throw err;
+
+        return res.status(200).send({
+            failure: 0,
+            message: result.length + " rental(s) returned",
+            result_table: result
+        })
+    });
+
+})
+
+app.post("/return_film", (req, res) => {
+    if(!/^[0-9]+$/.test(req.headers.customer_id))
+    {
+        return res.status(200).send({
+            failure: 1,
+            message: 'Customer ID needs to be a number.'
+        });
+    }
+    if(!/^[0-9]+$/.test(req.headers.rental_id))
+    {
+        return res.status(200).send({
+            failure: 1,
+            message: 'Rental ID needs to be a number.'
+        });
+    }
+    /*Ensure that the rental exists and is not already returned*/
+    value_count_in_column("rental", "rental_id", req.headers.rental_id).then( function(rental_count){
+    if(rental_count == 0)
+    {
+        return res.status(200).send({
+            failure: 1,
+            message: 'Rental ID corresponds to a non-existent rental.'
+        });
+    }
+    get_value_where("rental", "rental_id", req.headers.rental_id, "return_date").then( function(return_date){
+    if(return_date != null)
+    {
+        return res.status(200).send({
+            failure: 1,
+            message: "Rental has already been returned on " + return_date + "."
+        });
+    }
+    sql_string = "UPDATE rental SET return_date = CURRENT_TIMESTAMP() WHERE customer_id = " + req.headers.customer_id + ";";
+    con.query(sql_string, function (err, result, fields) {
+        if (err) 
+            throw err;
+
+        return res.status(200).send({
+            failure: 0,
+            message: "Film marked as returned.",
+            result_table: result
+        })
+    });
+
+    })});
+})
+
+
+app.get("/get_pdf_report", (req, res) => {
+
+    const doc = new PDFDocument(); 
+   
+    doc.pipe(fs.createWriteStream('test.pdf')); 
+    doc 
+       
+      .fontSize(27) 
+      .text('test', 100, 100); 
+    doc.end();
+})
+
 module.exports = app;
