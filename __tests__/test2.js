30c4c5cf27725f8da318b7c365330ee06705c192
Spencer Clarke <spence12310@icloud.com>
2023-10-12 01:13:17 -0400

    Final commit

--- __tests__/test2.js
+++ __tests__/test2.js
@@ -1,13 +1,13 @@
 let server_url="http://localhost:3500/";
-test_cases = [4,5,6];
+test_cases = [107,102,198];
 for(let i = 0; i < test_cases.length; i++)
 {
     test('get actor info test', async () => {
-        let response = await fetch(server_url + "get_film_info/",{
+        let response = await fetch(server_url + "get_actor_info/",{
             method: "GET",
             mode: "cors",
             headers: {
-                "film_id": test_cases[i]
+                "actor_id": test_cases[i]
             },
         })
         let data = await response.text();
@@ -15,13 +15,13 @@ for(let i = 0; i < test_cases.length; i++)
         switch(test_cases[i])
         {
             case 4:
-                expect(JSON.parse(data).result_table[0].title).toBe("AFFAIR PREJUDICE");
+                expect(JSON.parse(data).result_table[0].first_name).toBe("GINA");
                 break;
             case 5:
-                expect(JSON.parse(data).result_table[0].title).toBe("AFRICAN EGG");
+                expect(JSON.parse(data).result_table[0].title).toBe("WALTER");
                 break;
             case 6:
-                expect(JSON.parse(data).result_table[0].title).toBe("AGENT TRUMAN");
+                expect(JSON.parse(data).result_table[0].title).toBe("MARY");
                 break;
         }
     })
